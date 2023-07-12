0f129642d7eaf80d8721a533dd9b3f155b5681b7
Siddhuge <siddhanthuge@gmail.com>
2023-07-12 22:52:44 +0530

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          *$ REACT JS CONTAINER $*
+          *$ REACT JS CONTAINER on Feature Branch $*
         </p>
         <a
           className="App-link"
